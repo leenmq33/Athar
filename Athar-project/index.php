a3f925cd7153b876044c106571a4b42cd35f0b3f
Lu8na <Lubna.g19@gmail.com>
2026-03-16 19:53:09 +0300

    Create index.php

---
+++ Athar-project/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>أثر</title>
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body class="auth-body">
+
+<div class="auth-wrapper">
+
+<div class="auth-card" style="text-align:center;">
+
+<div class="logo-box">
+<img src="assets/images/logo.png" class="logo">
+</div>
+
+<h1 class="auth-title">مرحبًا بك في أثر</h1>
+
+<p class="auth-subtitle">
+منصة تسهّل التبرع ومشاركة الأثر الإيجابي في المجتمع
+</p>
+
+<div style="margin-top:25px;">
+
+<a href="login.php">
+<button class="main-btn">تسجيل الدخول</button>
+</a>
+
+</div>
+
+<div style="margin-top:15px;">
+
+<a href="register.php">
+<button class="main-btn" style="background:linear-gradient(135deg,#C8A96B,#B99855);">
+إنشاء حساب
+</button>
+</a>
+
+</div>
+
+</div>
+
+</div>
+
+</body>
+</html>
